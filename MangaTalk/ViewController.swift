31dff81acaa402d0df0c35ef4f5db6220808bdf7
Muyao Xu <xumuyao97@gmail.com>
2019-01-12 17:16:04 -0800

    Initial Commit

---
+++ MangaTalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MangaTalk
+//
+//  Created by Muyao Xu on 2019/1/12.
+//  Copyright © 2019 MangaTalk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
